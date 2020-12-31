4672b0746f5353a0ad44f9d1bb965224c2ad0b78
cchen117 <hchen117@terpmail.umd.edu>
2020-12-31 15:09:33 +0800

    Update and rename datepicker.js to datepicker-tw.js

--- ui/widgets/datepicker-tw.js
+++ ui/widgets/datepicker-tw.js
@@ -3,6 +3,9 @@
 * Includes: widget.js, position.js, data.js, disable-selection.js, effect.js, effects/effect-blind.js, effects/effect-bounce.js, effects/effect-clip.js, effects/effect-drop.js, effects/effect-explode.js, effects/effect-fade.js, effects/effect-fold.js, effects/effect-highlight.js, effects/effect-puff.js, effects/effect-pulsate.js, effects/effect-scale.js, effects/effect-shake.js, effects/effect-size.js, effects/effect-slide.js, effects/effect-transfer.js, focusable.js, form-reset-mixin.js, jquery-1-7.js, keycode.js, labels.js, scroll-parent.js, tabbable.js, unique-id.js, widgets/accordion.js, widgets/autocomplete.js, widgets/button.js, widgets/checkboxradio.js, widgets/controlgroup.js, widgets/datepicker.js, widgets/dialog.js, widgets/draggable.js, widgets/droppable.js, widgets/menu.js, widgets/mouse.js, widgets/progressbar.js, widgets/resizable.js, widgets/selectable.js, widgets/selectmenu.js, widgets/slider.js, widgets/sortable.js, widgets/spinner.js, widgets/tabs.js, widgets/tooltip.js
 * Copyright jQuery Foundation and other contributors; Licensed MIT */
 
+// import $ from 'jquery'
+// import 'jquery-ui/ui/widgets/datepicker'
+
 (function( factory ) {
 	if ( typeof define === "function" && define.amd ) {
 
@@ -7232,18 +7235,19 @@ function Datepicker() {
 	this._dayOverClass = "ui-datepicker-days-cell-over"; // The name of the day hover marker class
 	this.regional = []; // Available regional settings, indexed by language code
 	this.regional[ "" ] = { // Default regional settings
-		closeText: "Done", // Display text for close link
-		prevText: "Prev", // Display text for previous month link
-		nextText: "Next", // Display text for next month link
-		currentText: "Today", // Display text for current month link
-		monthNames: [ "January","February","March","April","May","June",
-			"July","August","September","October","November","December" ], // Names of months for drop-down and formatting
-		monthNamesShort: [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ], // For formatting
-		dayNames: [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ], // For formatting
-		dayNamesShort: [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ], // For formatting
-		dayNamesMin: [ "Su","Mo","Tu","We","Th","Fr","Sa" ], // Column headings for days starting at Sunday
-		weekHeader: "Wk", // Column header for week of the year
-		dateFormat: "mm/dd/yy", // See format options on parseDate
+		closeText: "關閉", // Display text for close link
+		prevText: "&#x3C;上個月", // Display text for previous month link
+		nextText: "下個月&#x3E;", // Display text for next month link
+		currentText: "今天", // Display text for current month link
+		monthNames: [ "一月","二月","三月","四月","五月","六月",
+		"七月","八月","九月","十月","十一月","十二月" ], // Names of months for drop-down and formatting
+		monthNamesShort: [ "一月","二月","三月","四月","五月","六月",
+		"七月","八月","九月","十月","十一月","十二月" ], // For formatting
+		dayNames: [ "星期日","星期一","星期二","星期三","星期四","星期五","星期六" ], // For formatting
+		dayNamesShort: [ "週日","週一","週二","週三","週四","週五","週六" ], // For formatting
+		dayNamesMin: [ "日","一","二","三","四","五","六" ], // Column headings for days starting at Sunday
+		weekHeader: "週", // Column header for week of the year
+		dateFormat: "yy/mm/dd", // See format options on parseDate
 		firstDay: 0, // The first day of the week, Sun = 0, Mon = 1, ...
 		isRTL: false, // True if right-to-left language, false if left-to-right
 		showMonthAfterYear: false, // True if the year select precedes month, false for month then year
